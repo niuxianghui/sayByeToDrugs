57c93be15e8cece70c33f699df76e1c732e142bc
RageYourDream <49909509@qq.com>
2018-05-11 20:03:16 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/observable/interval';
+import 'rxjs/add/operator/map';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('heroState', [
+      state('inactive', style({
+        backgroundColor: '#eee',
+        transform: 'scale(1)'
+      })),
+      state('active',   style({
+        backgroundColor: '#cfd8dc',
+        transform: 'scale(1.1)'
+      })),
+      transition('inactive => active', animate('100ms ease-in')),
+      transition('active => inactive', animate('100ms ease-out'))
+    ])
+  ]
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  sayByeTime = new Date('2018-5-8-22:00').getTime();
+
+  distance: Date;
+  day;
+  hour;
+  minute;
+  second;
+  ms;
+
+  state = 'inactive';
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    Observable.interval(1)
+      .map(() => {
+        const now = new Date();
+        const time = now.getTime() - this.sayByeTime;
+        return time;
+      })
+      .subscribe(time => {
+        const nTime = time / 1000;
+        this.day = Math.floor(nTime / 86400);
+        this.hour = Math.floor(nTime % 86400 / 3600);
+        this.minute = Math.floor(nTime % 86400 % 3600 / 60);
+        const second = Math.floor(nTime % 86400 % 3600 % 60);
+        if (second !== this.second) {
+          this.toggleState();
+        }
+        this.second = second;
+        this.ms = Math.floor(time % 1000);
+      });
+  }
+
+  toggleState() {
+    this.state = this.state === 'active' ? 'inactive' : 'active';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-
-import { AppComponent } from './app.component';
-
+import {AppComponent} from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
